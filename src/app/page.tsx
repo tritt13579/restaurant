184706e1169733fe57fc9cf893efab7f473e024c
Thanh Tri <tritt13579@gmail.com>
2024-03-04 00:29:35 +0700

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import Featured from "@/components/Featured";
+import Offer from "@/components/Offer";
+import Slider from "@/components/Slider";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>Hello world!</main>
+    <main>
+      <Slider />
+      <Featured />
+      <Offer />
+    </main>
   );
 }
